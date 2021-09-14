d8a999b95fff5eae2aff604cbe9544312dc4bb4e
Hildelbrando Correa Salcedo <hscorrea@unicesar.edu.co>
2021-09-14 18:09:47 -0500

    insercion de imagenes de presentacion

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementById("demo").innerHTML = this.responseText;
+      }
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  }
+
+
 document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
